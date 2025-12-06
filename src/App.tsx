18cf27fa7ca01de29cab6190dbfc3875c4dbb682
camel-x <projectcamelx@gmail.com>
2025-12-07 00:59:47 +0530

    Initial commit: Fixed blank page issue and updated React Router for subdirectory hosting

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
+import ImportantDates from './components/ImportantDates';
+import Committee from './components/comittee';
+import Registration from './components/Registration';
+import Journal from './components/Journal';
+import Mumbai from './components/Mumbai';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import PaperSubmission from './components/PaperSubmission';
+
+function App() {
+  return (
+    <Router basename="/smartssd">
+      <div className="min-h-screen">
+        <Header />
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Hero />
+              <About />
+              <ImportantDates />
+              <Committee />
+              <Registration />
+              <PaperSubmission />
+              <Journal />
+              <Mumbai />
+              <Contact />
+              <Footer />
+            </>
+          } />
+          <Route path="/papersubmission" element={
+            <>
+              <PaperSubmission />
+              <Footer />
+            </>
+          } />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
